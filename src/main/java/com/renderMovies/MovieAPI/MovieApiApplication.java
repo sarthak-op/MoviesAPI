e0fa72b81f7ae7c2ea6d135a8231db3f027c20d3
sarthak-op <kekatpure_sarthak.it@ghrce.raisoni.net>
2023-10-26 21:50:57 +0530

    cors configure

--- src/main/java/com/renderMovies/MovieAPI/MovieApiApplication.java
+++ src/main/java/com/renderMovies/MovieAPI/MovieApiApplication.java
@@ -1,11 +1,17 @@
 package com.renderMovies.MovieAPI;
 
+import com.github.alexdlaird.ngrok.NgrokClient;
+import com.github.alexdlaird.ngrok.protocol.CreateTunnel;
+import com.github.alexdlaird.ngrok.protocol.Tunnel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -13,9 +19,26 @@ public class MovieApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
+
+		final NgrokClient ngrokClient = new NgrokClient.Builder().build();
+		final CreateTunnel createTunnel = new CreateTunnel.Builder()
+				.withAddr(8080)
+				.build();
+		final Tunnel tunnel = ngrokClient.connect(createTunnel);
 	}
-
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 	@GetMapping("/")
 	public String Hello() {
 		return "Movie API's....Go to - url/api/v1/movies";
